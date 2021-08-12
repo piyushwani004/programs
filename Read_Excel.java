171afcb82abf37fc823290f0e90684ff1db76ea5
Piyush wani <56467741+piyushwani004@users.noreply.github.com>
2021-08-12 11:51:14 +0530

    Read excel file java program

---
+++ Read_Excel.java
@@ -0,0 +1,35 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Read_Excel {
+
+	public static void main(String[] args) {
+
+		File file = new File("file1.xlsx");
+
+		XSSFWorkbook wkbook;
+		XSSFSheet sheet;
+
+		try {
+			FileInputStream excel = new FileInputStream(file);
+			wkbook = new XSSFWorkbook(excel);
+			sheet = wkbook.getSheet("Sheet1");
+			int start = sheet.getFirstRowNum();
+			int end = sheet.getLastRowNum();
+			for(int i = start; i <= end; i++) {
+				System.out.print(sheet.getRow(i).getCell(0).getStringCellValue() + "\t");
+				System.out.print(sheet.getRow(i).getCell(1).getNumericCellValue() + "\t");
+				System.out.print(sheet.getRow(i).getCell(2).getBooleanCellValue() + "\t");
+				System.out.println(sheet.getRow(i).getCell(3).getStringCellValue());
+			}
+
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
